e18e2d336fc2e173509b364746358ecaec877e33
Allan Jardine <allan@datatables.net>
2022-10-10 19:51:05 +0000

    Add support for mjs files (ES Modules)

---
+++ js/searchBuilder.bulma.mjs
@@ -0,0 +1,27 @@
+
+/*! Bulma ui integration for DataTables' SearchBuilder
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-bm';
+import DataTable from 'datatables.net-searchbuilder';
+
+
+$.extend(true, DataTable.SearchBuilder.classes, {
+    clearAll: 'button dtsb-clearAll'
+});
+$.extend(true, DataTable.Group.classes, {
+    add: 'button dtsb-add',
+    clearGroup: 'button dtsb-clearGroup is-light',
+    logic: 'button dtsb-logic is-light'
+});
+$.extend(true, DataTable.Criteria.classes, {
+    container: 'dtsb-criteria',
+    "delete": 'button dtsb-delete',
+    left: 'button dtsb-left',
+    right: 'button dtsb-right'
+});
+
+
+export default DataTable;
