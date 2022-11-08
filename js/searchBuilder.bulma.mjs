58ecdfaa12353f305e1b8bb6b5a5f2c63451a765
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-08 14:11:45 +0000

    7702b412c3ac089c2c5c21039318218352233fb7 Release 1.4.0
    Sync to source repo @7702b412c3ac089c2c5c21039318218352233fb7

--- js/searchBuilder.bulma.mjs
+++ js/searchBuilder.bulma.mjs
@@ -1,11 +1,10 @@
-
 /*! Bulma ui integration for DataTables' SearchBuilder
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bm';
-import DataTable from 'datatables.net-searchbuilder';
+import 'datatables.net-searchbuilder';
 
 
 $.extend(true, DataTable.SearchBuilder.classes, {
