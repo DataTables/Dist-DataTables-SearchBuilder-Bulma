e02755956befd258d887e2cb238d4c2b4584d77e
dtbuild <dtbuild@rigil>
2026-05-07 14:01:03 +0000

    ae192b3a73e083969c7463e28f5904dc8930608f
    Sync to source repo @363f53b76e0441dbc1b2720ebe6f90cf3d008b35

--- js/searchBuilder.bulma.mjs
+++ js/searchBuilder.bulma.mjs
@@ -1,26 +1,22 @@
-/*! Bulma ui integration for DataTables' SearchBuilder
- * © SpryMedia Ltd - datatables.net/license
+/*! SearchBuilder Bulma styling 2.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-bm';
 import SearchBuilder from 'datatables.net-searchbuilder';
 
-// Allow reassignment of the $ variable
-let $ = jQuery;
-
-$.extend(true, DataTable.SearchBuilder.classes, {
+Object.assign(DataTable.SearchBuilder.classes, {
     clearAll: 'button dtsb-clearAll'
 });
-$.extend(true, DataTable.Group.classes, {
+Object.assign(DataTable.Group.classes, {
     add: 'button dtsb-add',
     clearGroup: 'button dtsb-clearGroup',
     logic: 'button dtsb-logic',
     search: 'button dtsb-search'
 });
-$.extend(true, DataTable.Criteria.classes, {
+Object.assign(DataTable.Criteria.classes, {
     container: 'dtsb-criteria',
-    "delete": 'button dtsb-delete',
+    delete: 'button dtsb-delete',
     left: 'button dtsb-left',
     right: 'button dtsb-right',
     input: 'input dtsb-input'
@@ -28,3 +24,4 @@ $.extend(true, DataTable.Criteria.classes, {
 
 
 export default DataTable;
+
