efd0316b1267bb0eac5a89f4e2da1659e6cbc8f6
dtbuild <dtbuild@rigil>
2026-07-22 14:50:54 +0000

    b2567bef1a733d021110553c86991410013af8de Release 2.0.0 ready
    Sync to source repo @b2567bef1a733d021110553c86991410013af8de

--- js/searchBuilder.bulma.min.mjs
+++ js/searchBuilder.bulma.min.mjs
@@ -1,4 +1,4 @@
-/*! SearchBuilder Bulma styling 2.0.0-beta.1 for DataTables
+/*! SearchBuilder Bulma styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bm";import SearchBuilder from"datatables.net-searchbuilder";Object.assign(DataTable.SearchBuilder.classes,{clearAll:"button dtsb-clearAll"}),Object.assign(DataTable.Group.classes,{add:"button dtsb-add",clearGroup:"button dtsb-clearGroup",logic:"button dtsb-logic",search:"button dtsb-search"}),Object.assign(DataTable.Criteria.classes,{container:"dtsb-criteria",delete:"button dtsb-delete",left:"button dtsb-left",right:"button dtsb-right",input:"input dtsb-input"});export default DataTable;
